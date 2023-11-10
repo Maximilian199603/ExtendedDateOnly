91b5f0687b21fc53fb2fa1276cd32a90afe56e44
Maximilian1996 <magnitudetwende@gmail.com>
2023-11-10 16:57:06 +0100

    Removed unnecesary using statements

--- ExtendedTimeUniTest/ExtendedTimeSpanUnitTest.cs
+++ ExtendedTimeUniTest/ExtendedTimeSpanUnitTest.cs
@@ -1,9 +1,4 @@
 ﻿using ExtendedDate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExtendedTimeUnitTest;
 internal class ExtendedTimeSpanUnitTest
